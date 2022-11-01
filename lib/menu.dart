040af21cfc191491962374035cef3d09e450eb90
Michal Predotka <mivoligo@gmail.com>
2022-11-01 10:56:12 +0000

    Add MenuItem

--- lib/menu.dart
+++ lib/menu.dart
@@ -6,11 +6,45 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const [
-        Text('Favorites'),
-        Text('Master bedroom'),
-        Text('Small bedroom'),
+      children: [
+        MenuItem(
+          name: 'Favourites',
+          onTap: () {},
+        ),
+        MenuItem(
+          name: 'Master bedroom',
+          onTap: () {},
+        ),
       ],
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  const MenuItem({
+    Key? key,
+    required this.name,
+    required this.onTap,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  final String name;
+  final VoidCallback onTap;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        child: Text(
+          name,
+          style: isSelected
+              ? const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
+              : const TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
