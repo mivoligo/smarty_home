02a06a7f45af0a7c4adbe3814f37d715529f94df
Michal Predotka <mivoligo@gmail.com>
2022-11-01 10:23:17 +0000

    Add Menu and PageContent

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'menu.dart';
+import 'page_content.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: const [
+        Menu(),
+        PageContent(),
+      ],
+    );
   }
 }
---
+++ lib/page_content.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageContent extends StatelessWidget {
+  const PageContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
